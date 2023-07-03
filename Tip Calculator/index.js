33e2200230598ed1884f659ed55c15d17261908a
Lee Sang Bong <108534975+bestcoolestp@users.noreply.github.com>
2023-07-03 15:31:43 +0900

    Tip Calculator

---
+++ Tip Calculator/index.js
@@ -0,0 +1,14 @@
+const btnEl = document.getElementById('calculate');
+const billEl = document.getElementById('bill');
+const tipEl = document.getElementById('tip');
+const totalEl = document.getElementById('total');
+
+
+function calculateTotal() {
+    const billValue = billEl.value;
+    const tipValue = tipEl.value;
+    const total = billValue * (1 + tipValue/100);
+    totalEl.innerText = total.toFixed(2);
+}
+
+btnEl.addEventListener('click', calculateTotal);
